e38e29bc5bdcfb5fc188c5880853304ddc0f3504
albert <avera@elpuig.xeill.net>
2019-11-11 20:23:30 +0100

    a

--- app/src/main/java/com/example/bacelonatours/MainViewModel.java
+++ app/src/main/java/com/example/bacelonatours/MainViewModel.java
@@ -1,6 +1,7 @@
 package com.example.bacelonatours;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -10,6 +11,14 @@ import androidx.lifecycle.MutableLiveData;
 import com.example.bacelonatours.api.BarceloninaApiModule;
 import com.example.bacelonatours.model.BarceloninaResponse;
 import com.example.bacelonatours.model.TourDetail;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,29 +26,49 @@ import retrofit2.Response;
 
 public class MainViewModel extends AndroidViewModel {
 
+    Application application;
     public String tourId;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
+        this.application = application;
     }
 
     public LiveData<BarceloninaResponse> obtenerTours(){
         final MutableLiveData<BarceloninaResponse> apiResponse = new MutableLiveData<>();
 
-        BarceloninaApiModule.barceloninaApi.obtenerTours().enqueue(new Callback<BarceloninaResponse>() {
-            @Override
-            public void onResponse(Call<BarceloninaResponse> call, Response<BarceloninaResponse> response) {
-                apiResponse.postValue(response.body());
-            }
+        try {
+            Gson gson = new Gson();
+            InputStream raw =  application.getResources().openRawResource(R.raw.bd);
+            Reader rd = new BufferedReader(new InputStreamReader(raw));
+            BarceloninaResponse data = gson.fromJson(rd, BarceloninaResponse.class);
 
-            @Override
-            public void onFailure(Call<BarceloninaResponse> call, Throwable t) {
-            }
-        });
+            apiResponse.setValue(data);
+
+        }catch (Exception e){
+            Log.e("ABCD", "Exception" + e.getMessage());
+        }
 
         return apiResponse;
     }
 
+//    public LiveData<BarceloninaResponse> obtenerTours(){
+//        final MutableLiveData<BarceloninaResponse> apiResponse = new MutableLiveData<>();
+//
+//        BarceloninaApiModule.barceloninaApi.obtenerTours().enqueue(new Callback<BarceloninaResponse>() {
+//            @Override
+//            public void onResponse(Call<BarceloninaResponse> call, Response<BarceloninaResponse> response) {
+//                apiResponse.postValue(response.body());
+//            }
+//
+//            @Override
+//            public void onFailure(Call<BarceloninaResponse> call, Throwable t) {
+//            }
+//        });
+//
+//        return apiResponse;
+//    }
+//
     public LiveData<TourDetail> obtenerTour(String id){
         final MutableLiveData<TourDetail> tourDetailMutableLiveData = new MutableLiveData<>();
 
